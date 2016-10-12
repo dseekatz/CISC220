27c52796a91aa99629bcde535e3166df49e3d2a2
dseekatz <dseekatz@gmail.com>
2016-10-12 13:50:21 -0400

    Added comments.  Hopefully the final version.

--- calculator.sh
+++ calculator.sh
@@ -3,6 +3,7 @@
 # A simple script that functions as a running calculator. It prompts the user to input a number, an operator,
 # and another number. It then performs the chosen operation and prompts the user for another operator and number, etc.
 
+echo "This is a simple calculator tool.  You can enter \":q\" at any time to exit."
 read -p "Please enter a number: " num1
 
 # The user can type :q at any time and the program will terminate
@@ -27,11 +28,11 @@ while true ; do
 	if [[ $num2 == ':q' ]] ; then
 		break							# Terminate the program
 	fi
-	case $oper in
-		[*]) (( num1=$num1*$num2 )) ;;
+	case $oper in							# If the user supplied a valid operator,
+		[*]) (( num1=$num1*$num2 )) ;;				# perform the desired operation
 		[/]) (( num1=$num1/$num2 )) ;;
 		[+]) (( num1=$num1+$num2 )) ;;
 		[-]) (( num1=$num1-$num2 )) ;;
 	esac
-	echo "The total so far is $num1"
+	echo "The total so far is $num1"				# show the user the total so far
 done
