bfcb3ca12f6a9bd37be765d29cbc502d4a40148a
brookealynn <13brr2@queensu.ca>
2016-10-12 14:56:39 -0500

    hello

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,1 +1,18 @@
 permissionsResetter.sh
+#!/bin/bash
+
+echo "Press '-s' to show the files and directories that had their permissions changed along with the old and current permissions"
+echo "Press '-r' to reset the permissions of the files and directories that had their permissions changed to the old permissions"
+#pass in arguments
+$/permissionsResetter.sh -s -r
+
+if [$1 == '-s'];
+echo stat question4.sh #or ls -lt
+else if [$1 == 'r'];
+
+ 
+ 
+
+
+
+
