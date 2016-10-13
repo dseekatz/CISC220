cb69d070ec28ac39be0839dbc924b6f0ec9810c7
brookealynn <13brr2@queensu.ca>
2016-10-13 16:00:54 -0500

    ok

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -3,14 +3,32 @@ permissionsResetter.sh
 
 echo "Press '-s' to show the files and directories that had their permissions changed along with the old and current permissions"
 echo "Press '-r' to reset the permissions of the files and directories that had their permissions changed to the old permissions"
-#pass in arguments
+#pass in awrguments
 $/permissionsResetter.sh -s -r
 
 if [$1 == '-s'];
-echo stat question4.sh #or ls -lt
-else if [$1 == 'r'];
+echo "OLD" "CURRENT" "FILE"
+for line in [ $ (diff file1.sh file2.sh);]
+	#create a counter to store 3 variables to store permissions
+	counter=0	 
+	if grep <*  #need to extract just the permissions
+	oldpermissions[counter] =  ${$line:0:10}
+	else grep >*
+	newpermissions[counter] = ${$line:0:10} 
+	filenames[counter] = ${$line:11}
+for i in `seq 0  ${#filenames[@]}`
+echo -ne oldpermissions[i]  #to get columns
+echo -ne newpermissions[i]
+echo -ne filenames[i]
+echo  "\n"
 
- 
+	
+
+
+
+
+#else if [$1 == 'r'];
+
  
 
 
