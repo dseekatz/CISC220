aa065c5aae52b3f7a39680777bc890c6c54266b6
brookealynn <13brr2@queensu.ca>
2016-10-13 16:20:57 -0500

    try again

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,35 +1,33 @@
-permissionsResetter.sh
+function permissionsResetter $1 $2 { 
 #!/bin/bash
 
-echo "Press '-s' to show the files and directories that had their permissions changed along with the old and current permissions"
-echo "Press '-r' to reset the permissions of the files and directories that had their permissions changed to the old permissions"
-#pass in awrguments
-$/permissionsResetter.sh -s -r
-
-if [$1 == '-s'];
+if [$1 == '-s']; then
 echo "OLD" "CURRENT" "FILE"
 for line in [ $ (diff file1.sh file2.sh);]
 	#create a counter to store 3 variables to store permissions
-	counter=0	 
-	if grep <*  #need to extract just the permissions
-	oldpermissions[counter] =  ${$line:0:10}
-	else grep >*
-	newpermissions[counter] = ${$line:0:10} 
-	filenames[counter] = ${$line:11}
+	counter=0
+	fi	 
+	if [[ grep <* ]]; then #need to extract ;ust the permissions
+		oldpermissions[counter] =  ${$line:0:10}
+	else if [[ grep >* ]]
+		newpermissions[counter] = ${$line:0:10} 
+		filenames[counter] = ${$line:11}
+	fi
+done
 for i in `seq 0  ${#filenames[@]}`
-echo -ne oldpermissions[i]  #to get columns
-echo -ne newpermissions[i]
-echo -ne filenames[i]
-echo  "\n"
-
+	echo -ne oldpermissions[i]  #to get columns
+	echo -ne newpermissions[i]
+	echo -ne filenames[i]
+	echo  "\n"
+done 
 	
 
-
+}
 
 
 #else if [$1 == 'r'];
 
- 
+
 
 
 
