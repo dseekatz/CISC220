24800fd359f01139757eaef5a81187f198a8686a
sujay <sujay.ngrj@gmail.com>
2016-10-15 00:52:58 -0400

    work in progress

--- Question3Bonus.sh
+++ Question3Bonus.sh
@@ -2,37 +2,47 @@
 echo "Welcome to CISC220 Racing Arena"
 echo "How many players?"
 
-read users
-for i in $(seq $users)
+read input
+
+users=()
+spaces=()
+
+for i in $(seq $input); 
 do
-	echo "User $i press $i to move forward"
+        echo "User $i press $i to move forward"
+        ${users[i]}="|->"
+        {spaces[i]}=40
 done
 
-for i in $(seq $users)
-do
+continue=0
 
-	car$i="|->"
-	space$i=40
+while ((${continue}==0)) ; do
+
+        for i in $(seq $input); do
+		if [spaces[$i] == 0]; then
+                        continue=1
+                fi
+        done
 
 
-while (( ${#car?} < 43 )) ; do
-	read num
-	clear
-	echo "Welcome to CISC220 Racing Arena"
-	echo "User 1 press 1 to move forward, User 2 press 2 and User 3 press 3"
+        read num
+        clear
+        echo "Welcome to CISC220 Racing Arena""
 
-	for i in $(seq $users)
-	do
+        for i in $(seq $users);
+        do
         	echo "User $i press $i to move forward"
-		car$i="~$car$1"
-		space$i=$((space$i-1))
-		line$i=$(printf "%-${space$i}s" " ")
-	echo $car$1 "$line$1# Lane 1 #"
-done
+                if [$num == $i]; then
+                        users[$i]="~users[$i]"
+                        spaces[$i]=((spaces[$i]-1))
+                        line=$(printf "%-${spaces[$i]}s" " ")
+                        echo "users[$i]" "$line# Lane $i"
+                fi
+        done
 done
 for i in $(seq $users) do
 
-	if [ ${#car$i} == 43 ] ; then
-		echo "Player $i Wins!"
-	fi
+        if [ ${#car$i} == 43 ]; then
+                echo "Player $i Wins!"
+        fi
 done
