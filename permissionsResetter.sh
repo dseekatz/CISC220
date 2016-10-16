87611c39d653805382011e69851ede67ae0948be
brookealynn <13brr2@queensu.ca>
2016-10-16 12:34:50 -0500

    trying again

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,31 +1,29 @@
-function permissionsResetter $1 $2 { 
+
 #!/bin/bash
 
-if [$1 == '-s']; then
+if [ "$1" = '-s' ]; then
 echo "OLD" "CURRENT" "FILE"
-for line in [ $ (diff file1.sh file2.sh);]
+	for line in [[  $(diff file1.sh file2.sh) ]]; do
 	#create a counter to store 3 variables to store permissions
-	counter=0
-	fi	 
-	if [[ grep <* ]]; then #need to extract ;ust the permissions
-		oldpermissions[counter] =  ${$line:0:10}
-	else if [[ grep >* ]]
-		newpermissions[counter] = ${$line:0:10} 
-		filenames[counter] = ${$line:11}
-	fi
-done
-for i in `seq 0  ${#filenames[@]}`
-	echo -ne oldpermissions[i]  #to get columns
-	echo -ne newpermissions[i]
-	echo -ne filenames[i]
+ 	#counter=0	 
+		if [[ grep <* ]]; then #need to extract ;ust the permissions
+			oldpermissions[counter] =  ${$line:0:10}
+		else [[ grep >* ]]
+			newpermissions[counter] = ${$line:0:10}
+			filenames[counter] = ${$line:11}
+		fi
+	done
+fi
+
+for i in `seq 0  ${#filenames[@]}` do
+	if [ "$newpermissions[$i]" -ne "$oldpermissions[$i]" ]; then  #to get columns
+	echo filenames[i] | column | newpermissions | column | oldpermissions
 	echo  "\n"
+fi
 done 
 	
-
-}
-
-
-#else if [$1 == 'r'];
+#elif [ "$1" == 'r ' ];
+#chmod  
 
 
 
