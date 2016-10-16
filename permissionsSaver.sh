0f49c7d0d4531d4d830421270b21871f6a336a29
dseekatz <dseekatz@gmail.com>
2016-10-16 13:06:42 -0400

    added comments

--- permissionsSaver.sh
+++ permissionsSaver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-targetFile="savedPermissions"
+targetFile="savedPermissions"	# Initialize the name of the file that will contain the saved permissions
 counter=1
-while [ -e $targetFile ] ; do
+while [ -e $targetFile ] ; do	# If the target file exists, change the name of the target file and try again...
 	if [[ $counter == 1 ]] ; then
 		targetFile="$targetFile$counter" ;
 	else
@@ -10,5 +10,5 @@ while [ -e $targetFile ] ; do
 	fi
 	(( counter=$counter+1 )) ;
 done
-ls -l | awk 'NR>1 {print $1 "\t" $9}' > $targetFile
-echo "$targetFile created!"
+ls -l | awk 'NR>1 {print $1 "\t" $9}' > $targetFile	# Take the first and ninth columns of the list of files
+echo "$targetFile created!"				# and write them to the target file, ignoring the first line
