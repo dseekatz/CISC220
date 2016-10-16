d3a7b174e64b21a59899693a3ff227da050198c3
sujaynagaraj <sujay.ngrj@gmail.com>
2016-10-16 12:08:26 -0400

    Update Question3Bonus.sh

--- Question3Bonus.sh
+++ Question3Bonus.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 echo "Welcome to CISC220 Racing Arena"
 echo "How many players?"
 
@@ -17,24 +18,25 @@ done
 continue=0
 
 while (( ${continue} == 0 )) ; do
-
-
-
-        read num
-        clear
+	clear
         echo "Welcome to CISC220 Racing Arena"
 
         for i in $(seq $users);
         do
         	echo "User $i press $i to move forward"
-                if (( ${num} == $i )); then
-                        users[$i]="~${users[$i]}"
-                        spaces[$i]=$((spaces[$i]-1))
-                fi
-	line=$(printf "%-${spaces[$i]}s" " ")
-        echo "users[$i]" "$line# Lane $i"
-        done
+                #if (( ${num} == $i )); then
+                        #users[$i]="~${users[$i]}"
+                       	#spaces[$i]=$((spaces[$i]-1))
+                #fi
+		line=$(printf "%-${spaces[$i]}s" " ")
+       		echo "users[$i]" "$line# Lane $i"
+	done
 	
+	
+	read num
+	
+	users[$num]="~${users[$num]}"
+	spaces[$num]=$((spaces[$num]-1))
 	
 	for i in $(seq $input); do
 		if (( ${#users[$i]} == 43 )); then
