cba758ed08b7096cffe6efa436a15aa68b32b238
sujaynagaraj <sujay.ngrj@gmail.com>
2016-10-16 11:22:24 -0400

    Update Question3Bonus.sh

--- Question3Bonus.sh
+++ Question3Bonus.sh
@@ -10,16 +10,16 @@ spaces=()
 for i in $(seq $input); 
 do
         echo "User $i press $i to move forward"
-        ${users[i]}="|->"
-        ${spaces[i]}=40
+        users[$i]="|->"
+        spaces[$i]=40
 done
 
 continue=0
 
-while ((${continue}==0)) ; do
+while (({$continue}==0)) ; do
 
         for i in $(seq $input); do
-		if [${spaces[$i]} == 0]; then
+		if [${#users[$i]} == 43]; then
                         continue=1
                 fi
         done
@@ -32,8 +32,8 @@ while ((${continue}==0)) ; do
         for i in $(seq $users);
         do
         	echo "User $i press $i to move forward"
-                if [$num == $i]; then
-                        ${users[$i]}="~${users[$i]}"
+                if [[${num} == $i]]; then
+                        users[$i]="~${users[$i]}"
                         spaces[$i]=((spaces[$i]-1))
                         line=$(printf "%-${spaces[$i]}s" " ")
                         echo "users[$i]" "$line# Lane $i"
