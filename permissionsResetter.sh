ac390f9639435fc97e096fd11eb7d73a3f364574
brookealynn <13brr2@queensu.ca>
2016-10-16 13:53:47 -0500

    me again

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -1,29 +1,30 @@
-
 #!/bin/bash
 
 if [ "$1" = '-s' ]; then
 echo "OLD" "CURRENT" "FILE"
-	for line in [[  $(diff file1.sh file2.sh) ]]; do
-	#create a counter to store 3 variables to store permissions
- 	#counter=0	 
-		if [[ grep <* ]]; then #need to extract ;ust the permissions
-			oldpermissions[counter] =  ${$line:0:10}
+	for line in [[ $(diff <($2) <(ls -l | awk NR> 1 {print $1 "\t" $9) ]] ; do	 
+		if [[ grep <* ]]; then #need to extract just the permissions
+			oldpermissions =  ${$line:0:10}
 		else [[ grep >* ]]
-			newpermissions[counter] = ${$line:0:10}
-			filenames[counter] = ${$line:11}
+			newpermissions = ${$line:0:10}
+			filenames = ${$line:11}
 		fi
 	done
 fi
 
-for i in `seq 0  ${#filenames[@]}` do
-	if [ "$newpermissions[$i]" -ne "$oldpermissions[$i]" ]; then  #to get columns
-	echo filenames[i] | column | newpermissions | column | oldpermissions
+for i in ` seq 0  ${#filenames[i]} `; do
+	if [ "${newpermissions[$i]}" -ne "${oldpermissions[$i]}" ]; then  #to get columns
+	echo filenames[i] | column | newpermissions | column | oldpermissions | column
 	echo  "\n"
 fi
 done 
 	
-#elif [ "$1" == 'r ' ];
-#chmod  
+if [ "$1" == 'r ' ]; then
+	for i in `seq ${#filenames[i]} `; do
+	chmod oldpermissions[$i] filenames[$i] 
+done   
+fi 
+
 
 
 
